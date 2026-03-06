e70e92d1006027e9a2a0cf18217108a861252243
gabrielcecconello <gabrielcecconello@hotmail.com>
2026-03-06 14:24:15 -0300

    feat: first definitions and functions

---
+++ chip8.h
@@ -0,0 +1,21 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include <stdint.h>
+#include "definitions.h"
+
+typedef struct {
+    uint8_t memory[MEMORY_SIZE];
+    uint8_t display[SCREEN_WIDTH][SCREEN_HEIGHT];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    uint8_t v_registers[16];
+    uint16_t pc;
+    uint16_t index_reg;
+    uint16_t stack[16];
+    uint16_t stack_pointer;
+} Chip8;
+
+void chip8_init(Chip8 *chip8);
+void chip8_load_rom(Chip8 *chip8);
+#endif
---
+++ definitions.h
@@ -0,0 +1,9 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define MEMORY_SIZE 4096
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+#define START_ADDRESS 0x200
+
+#endif
